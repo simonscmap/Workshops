0b644a5723b152950a6b30be39ae00a710c86f95
mdashkezari <demo@mit.edu>
2020-06-06 21:48:32 -0700

    stack plots

--- CBIOMES2020/python/comparePlot.py
+++ CBIOMES2020/python/comparePlot.py
@@ -24,7 +24,26 @@ def normalize(vec, MinMax=True):
     return scaler.fit_transform(vec)
 
 
-def plot_double_axis(x, obs, mod, xLabel, obsLabel, modLabel, title):
+def normalize_obs_model(df, obsCol, modCol):    
+    """
+    Normalize the observation and model values.
+
+    Paratmeters:
+    ================
+    :param dataframe df: the dataframe containing the model and observation values. 
+    :param int obsCol: the index of column at the data file holding observations.
+    :param int modCol: the index of column at the data file holding model estimates.
+    """
+
+    obs = df.iloc[:, [obsCol]].values.astype(float)    
+    mod = df.iloc[:, [modCol]].values.astype(float)    
+    df.iloc[:, [obsCol]] = normalize(obs, MinMax=True) 
+    df.iloc[:, [modCol]] = normalize(mod, MinMax=True) 
+    return df
+
+
+
+def plot_double_axis(x, obs, mod, xLabel, obsLabel, modLabel, title, figDir):
     """
     Make a double-axis plot comparing observation and model.
 
@@ -79,7 +98,7 @@ def plot_double_axis(x, obs, mod, xLabel, obsLabel, modLabel, title):
     return
 
 
-def hist(df, title):
+def hist(df, title, figDir):
     """
     Make a histogram plot comparing the density distributions of model and observation data.
     The histograms are superimposed by Kernel Density Estimate (KDE) plots computed by Gaussian kernels.
@@ -107,7 +126,7 @@ def hist(df, title):
 
 
 
-def plot_metrics(vars, corr, mse, evs):
+def plot_metrics(vars, corr, mse, evs, figDir):
     """
     Make a histogram plot comparing the density distributions of model and observation data.
     The histograms are superimposed by Kernel Density Estimate (KDE) plots computed by Gaussian kernels.
@@ -132,62 +151,205 @@ def plot_metrics(vars, corr, mse, evs):
 
 
 
+def stackFiles(obsCol, modCol, files):
+    """
+    Take a list of files containing the observation and model-estimated values
+    corresponding to the specified species and stack them vertically.
+
+    Paratmeters:
+    ================
+    :param int obsCol: the index of column at the data file holding observations.
+    :param list x: list of file names where the observation and model data are stored.
+    """
+
+    stacked = pd.DataFrame({})
+    for fname in files:
+        df = pd.read_csv(fname)
+        df.rename(columns={df.columns[obsCol]: "obs"}, inplace=True)
+        df.dropna(how='any', subset=[df.columns[obsCol], df.columns[modCol]], inplace=True)
+        if len(df) < 1: continue
+        df = normalize_obs_model(df, obsCol, modCol)
+        if len(stacked) < 1: 
+            stacked = df
+            continue
+        stacked = pd.concat([stacked, df],ignore_index=True)
+    return stacked    
 
 
 
-
-
+def plot_aggregated_err_by_depth(df, figDir):
+    depth = [0, 5, 10, 20, 40, 100, 200, 500, 1000, 5000]
+    xs, errs, samples = [], [], []
+    for i in range(1, len(depth)):
+        sub = df.query("depth>=%f and depth<%f" % (depth[i-1], depth[i]))
+        if len(sub) < 1: continue
+        errs.append(sub.err.mean())
+        xs.append("%d_%d" % (depth[i-1], depth[i]))
+        samples.append(len(sub))
+    plt.clf()
+    _, ax1 = plt.subplots()
+    ax1.plot(xs, errs, "bo", zorder=0)
+    ax1.tick_params(axis="y", labelcolor='b') 
+    ax1.set_xlabel("depth [m]")
+    ax1.set_ylabel("obs - model", color='b')
+    plt.xticks(rotation=20)
     
-species = "proch"
-files = glob.glob("./data/%s/*.csv" % species)
-obsCol = 4   # observation values are at the fifth column (index 4)
-modCol = 6   # model values are at the seventh column 
-obsVars, corr, mse, evs = [], [], [], []
+    ax2 = ax1.twinx() 
+    ax2.bar(xs, samples, color="r", alpha=0.1)
+    ax2.set_ylabel('sample size', color='r')  
+    ax2.tick_params(axis="y", labelcolor='r')  
+    ax2.set_yscale('log')  
+    
+    plt.tight_layout()
+    plt.savefig("%sstack_err_depth.png" % figDir, dpi=300)
+    plt.close()    
+    return
 
 
-figDir = "./fig/"
-if not os.path.exists(figDir): os.makedirs(figDir)
-for fIndex, fName in enumerate(files):
-    print(fName)
-    df = pd.read_csv(fName)    
-    varName = os.path.splitext(os.path.basename(fName))[0]
 
-    ## remove rows with missing values
-    df.dropna(how='any', subset=[df.columns[obsCol], df.columns[modCol]], inplace=True)
-    if len(df) < 1: continue
-
-
-    ## normalize
-    obs = df.iloc[:, [obsCol]].values.astype(float)    
-    mod = df.iloc[:, [modCol]].values.astype(float)    
-    df.iloc[:, [obsCol]] = normalize(obs, MinMax=True) 
-    df.iloc[:, [modCol]] = normalize(mod, MinMax=True) 
-
-
-    obsVars.append(varName)
-    ## data-model correlation
-    corr.append(df.iloc[:, [obsCol, modCol]].corr(method="spearman").iloc[0, 1])
-    ## mse
-    mse.append(mean_squared_error(df.iloc[:, [obsCol]], df.iloc[:, [modCol]]))
-    ## explained_variance_score
-    evs.append(explained_variance_score(df.iloc[:, [obsCol]], df.iloc[:, [modCol]]))
-
-    if fIndex == len(files)-1:
-        plot_metrics(obsVars, corr, mse, evs)     
-
-    # title = varName
-    # hist(df.iloc[:, [obsCol, modCol]], title)
-    # xColLabel = "time"
+def plot_aggregated_err_by_lat(df, figDir):
+    lat = np.arange(-90, 90.1, 10)
+    xs, errs, samples = [], [], []
+    for i in range(1, len(lat)):
+        sub = df.query("lat>=%f and lat<%f" % (lat[i-1], lat[i]))
+        if len(sub) < 1: continue
+        errs.append(sub.err.mean())
+        xs.append("%d_%d" % (lat[i-1], lat[i]))
+        samples.append(len(sub))
+    plt.clf()
+    _, ax1 = plt.subplots()
+    ax1.plot(xs, errs, "bo", zorder=0)
+    ax1.tick_params(axis="y", labelcolor='b') 
+    ax1.set_xlabel("latitude [deg]")
+    ax1.set_ylabel("obs - model", color='b')
+    plt.xticks(rotation=40)
+    
+    ax2 = ax1.twinx() 
+    ax2.bar(xs, samples, color="r", alpha=0.1)
+    ax2.set_ylabel('sample size', color='r')  
+    ax2.tick_params(axis="y", labelcolor='r')  
+    ax2.set_yscale('log')  
+    
+    plt.tight_layout()
+    plt.savefig("%sstack_err_lat.png" % figDir, dpi=300)
+    plt.close()    
+    return
 
 
-    # if fName.find("tblSeaFlow") != -1: continue  # takes long time to plot seaflow data
-    # plot_double_axis(
-    #                 x=df[xColLabel], 
-    #                 obs=df.iloc[:, [obsCol]], 
-    #                 mod=df.iloc[:, [modCol]], 
-    #                 xLabel=xColLabel, 
-    #                 obsLabel=df.columns[obsCol] + df.iloc[0, 5], 
-    #                 modLabel=df.columns[modCol] + df.iloc[0, 8], 
-    #                 title=title
-    #                 )                             
+def plot_aggregated_err_by_month(df, figDir):
+    df["month"] = pd.to_datetime(df["time"]).dt.month
+    df = df.query("lat>=0")
+    df = df.groupby("month").agg({"lat": "size", "err": "mean"}).rename(columns={"lat": "count", "err": "err"}).reset_index()
 
+    plt.clf()
+    _, ax1 = plt.subplots()
+    ax1.plot(df.month, df.err, "bo", zorder=0)
+    ax1.tick_params(axis="y", labelcolor='b') 
+    ax1.set_xlabel("month")
+    ax1.set_ylabel("obs - model", color='b')
+    ax2 = ax1.twinx() 
+    ax2.bar(df.month, np.array(df["count"]), color="r", alpha=0.1)
+    ax2.set_ylabel('sample size', color='r')  
+    ax2.tick_params(axis="y", labelcolor='r')      
+    plt.tight_layout()
+    plt.savefig("%sstack_err_month.png" % figDir, dpi=300)
+    plt.close()    
+    return
+
+
+
+def stacked_figures(obsCol, modCol, files, figDir):
+    df = stackFiles(obsCol, modCol, files)
+    df["err"] = (df[df.columns[obsCol]] - df[df.columns[modCol]]) #/ df[df.columns[obsCol]]
+    # df = df[~df.isin([np.nan, np.inf, -np.inf]).any(1)]
+
+    hist(df.iloc[:, [obsCol, modCol]], 'stacked', figDir)
+    plot_aggregated_err_by_depth(df, figDir)
+    plot_aggregated_err_by_lat(df, figDir)
+    plot_aggregated_err_by_month(df, figDir)
+    return
+
+
+
+def make_dataset_figures(obsCol, modCol, files, figDir):
+    """
+    Take a list of files containing the observation and model-estimated values
+    corresponding to the specified species and create plots comparing the 
+    observations with model estimates.
+
+    Paratmeters:
+    ================
+    :param string species: the name of species.
+    :param int obsCol: the index of column at the data file holding observations.
+    :param int modCol: the index of column at the data file holding model estimates.
+    :param list x: list of file names where the observation and model data are stored.
+    """
+
+    obsVars, corr, mse, evs = [], [], [], []
+    for fIndex, fName in enumerate(files):
+        print(fName)
+        df = pd.read_csv(fName)    
+        varName = os.path.splitext(os.path.basename(fName))[0]
+
+        ## remove rows with missing values
+        df.dropna(how='any', subset=[df.columns[obsCol], df.columns[modCol]], inplace=True)
+        if len(df) < 1: continue
+
+        ## normalize
+        df = normalize_obs_model(df, obsCol, modCol)
+
+        obsVars.append(varName)
+        ## data-model correlation
+        corr.append(df.iloc[:, [obsCol, modCol]].corr(method="spearman").iloc[0, 1])
+        ## mse
+        mse.append(mean_squared_error(df.iloc[:, [obsCol]], df.iloc[:, [modCol]]))
+        ## explained_variance_score
+        evs.append(explained_variance_score(df.iloc[:, [obsCol]], df.iloc[:, [modCol]]))
+
+        if fIndex == len(files)-1:
+            plot_metrics(obsVars, corr, mse, evs, figDir)     
+
+        title = varName
+        hist(df.iloc[:, [obsCol, modCol]], title, figDir)
+        xColLabel = "time"
+
+        if fName.find("tblSeaFlow") != -1: continue  # takes long time to plot seaflow data
+        plot_double_axis(
+                        x=df[xColLabel], 
+                        obs=df.iloc[:, [obsCol]], 
+                        mod=df.iloc[:, [modCol]], 
+                        xLabel=xColLabel, 
+                        obsLabel=df.columns[obsCol] + df.iloc[0, 5], 
+                        modLabel=df.columns[modCol] + df.iloc[0, 8], 
+                        title=title,
+                        figDir=figDir
+                        )                             
+
+
+
+
+
+
+
+
+
+
+
+###########################################
+#                                         # 
+#                  main                   # 
+#                                         # 
+###########################################
+
+
+
+
+if __name__ == "__main__":
+    species = "syne"
+    files = glob.glob("./data/%s/*.csv" % species)
+    obsCol = 4   # observation values are at the fifth column (index 4)
+    modCol = 6   # model values are at the seventh column 
+    figDir = "./fig/"
+    if not os.path.exists(figDir): os.makedirs(figDir)
+    stacked_figures(obsCol, modCol, files, figDir)
+    # make_dataset_figures(obsCol, modCol, files, figDir)
